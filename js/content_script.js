6bf2966894680b3e64b9d618c9d2f356fcb943b4
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2011-12-18 16:54:04 +0900

    chrome裏側の処理

---
+++ js/content_script.js
@@ -0,0 +1,62 @@
+//------------------------------------------
+// content_script.js
+//------------------------------------------
+getPageInfo();
+
+// タブが切り替わったときの処理 
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+        getPageInfo();
+    }
+);
+
+// ページ情報取得処理
+function getPageInfo() {
+    // get page title
+    var title = document.getElementsByTagName('title')[0].firstChild.nodeValue.replace(/(^\s+)|(\s+$)/g, "");;
+    var imgurls = getImgUrls();
+console.log('title = ' + title);
+console.log('url = ' + location.href);
+    
+    // send background
+    var port = chrome.extension.connect({name: "frustration"});
+    port.postMessage({
+        title: title, 
+        url : location.href,
+        imgurls : imgurls,
+        status : "start"
+    });
+console.log("start post");
+
+    port.onMessage.addListener(function(msg) {
+
+        if (msg.status == "loading"){
+console.log("loading");
+            port.postMessage({status: "loading"});
+        }
+    });
+
+}
+// get urls in image tag
+function getImgUrls() {
+    var urls = new Array();
+    var size = 0;
+    elms = document.getElementsByTagName( "img" );
+    for (var i=0; i < elms.length; i++) {
+
+        // あまりにも小さい画像は取得しないようにする
+        if (elms[i].naturalHeight < 100) {
+            continue;
+        }
+        if (elms[i].naturalWidth < 100) {
+            continue;
+        }
+
+        urls.push( elms[i].src );
+        if (i > 100) {
+            break;
+        }
+    }
+
+    return urls;
+}
